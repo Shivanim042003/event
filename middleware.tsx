792b335ec78ad94dbe55074f3b730c9c3f99797f
Shivanim042003 <shivanim042003@gmail.com>
2024-04-22 15:18:15 +0530

    first commit

---
+++ middleware.tsx
@@ -0,0 +1,22 @@
+import { authMiddleware } from "@clerk/nextjs/server";
+
+export default authMiddleware({
+  publicRoutes: [
+    '/',
+    '/events/:id',
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ],
+  ignoredRoutes: [
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing',
+    '/favicon.ico',
+    '/assets/images/logo.svg'
+  ]
+});
+
+export const config = {
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
