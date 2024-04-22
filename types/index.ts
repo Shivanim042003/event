7c22eb3f0ac361c4abd6132d9c45974b40252e9d
Shivanim042003 <shivanim042003@gmail.com>
2024-04-22 16:41:53 +0530

    first commit

--- types/index.ts
+++ types/index.ts
@@ -1,20 +1,18 @@
 // ====== USER PARAMS
 export type CreateUserParams = {
     clerkId: string
-    firstName: string
-    lastName: string
+  
     username: string
     email: string
     photo: string
   }
   
   export type UpdateUserParams = {
-    firstName: string
-    lastName: string
+ 
+
     username: string
     photo: string
   }
-  
   // ====== EVENT PARAMS
   export type CreateEventParams = {
     userId: string
